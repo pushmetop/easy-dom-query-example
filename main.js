10c61e629067b64acb16811a4619b12fbf10dab7
zhangxiangliang <admin@pushme.top>
2021-05-27 11:37:48 +0800

    feat: 修改主包名称和增加测试用例

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
-import './style.css'
+import './style.css';
+import $ from 'easy-dom-query';
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+// document.querySelector('#app').innerHTML = `
+//   <h1>Hello Vite!</h1>
+//   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+// `
+
+// 测试选择函数
+const app = $('#app');
+console.log('测试选择函数', app);
+
+// 测试DOM函数
+console.log('测试DOM函数', $(app));
+
+// 测试 ready 函数
+$(() => console.log('测试 ready 函数', 'hello'));
