f01757da8208e8367c9c273c0a3a754dbfb99e67
zhangxiangliang <admin@pushme.top>
2021-05-27 14:07:17 +0800

    feat: 增加测试用例和代码结构

--- main.js
+++ main.js
@@ -1,23 +1,42 @@
 import './style.css';
 import $ from 'easy-dom-query';
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+// ---------- Query Selector Start ----------
 
-// 测试选择函数
-const app = $('#app');
-console.log('测试选择函数', app);
+// id 查询
+console.log('class 查询', $('#app'));
 
-// 测试DOM函数
-console.log('测试DOM函数', $(app));
+// class 查询
+console.log('id 查询', $('.header'));
 
-// 测试 ready 函数
-$(() => console.log('测试 ready 函数', 'ready'));
+// 选择器查询
+console.log('选择器查询', $('a'));
 
-// 测试事件函数
-$('#app').click(() => console.log('测试事件函数', 'click'));
+// 属性查询
+console.log('属性查询', $('a[target=_blank]'));
 
-// 测试事件函数
-$('#app').on('click', () => console.log('测试事件函数', 'on'));
+// ---------- Query Selector End----------
+
+
+// ---------- Event Start ----------
+
+// document ready
+$(() => console.log('document ready'));
+
+// click 事件
+$('#app').click(() => console.log('click 事件'));
+
+// on 绑定事件
+$('#app').on('click', () => console.log('on 绑定事件'));
+
+// off 解绑事件
+const onceEvent = () => {
+  console.log('off 解绑事件');
+  $('#app').off('click', onceEvent);
+};
+
+$('#app').on('click', offEvent);
+// ---------- Event End ----------
+
+
+
